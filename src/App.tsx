876f78cddac9db7c0e189f014c31e6f4fbe3642d
piyushgupte <piyushgupte@outlook.com>
2023-09-25 12:43:02 +0530

    Init the-cricketers-app
    This commit initialises the-cricketers-app which is created in vite using vite-ts template.
    
    - used a default vite-ts app as a boilerplate
    
    authored-by: Piyush Gupte <piyushgupte@outlook.com>

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import getPlayers from './server/get-players';
+// import { TPlayer } from './server/types';
+
+ function App() {
+  
+
+ 
+  // const getPlayersFromData =  async () => { 
+  //   const players: TPlayer[] = await getPlayers();
+  //       return players;
+  //     }
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() =>{ 
+         // console.log(getPlayersFromData())
+          }}>
+         List of players are in the logs
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
