60645f45fea1e21897cde1ecb66af698c1873938
piyushgupte <piyushgupte@outlook.com>
2023-10-06 10:42:55 +0530

    Implemented persistent reducers
    -application persist state on refresh

--- src/main.tsx
+++ src/main.tsx
@@ -8,13 +8,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 
-
+let persistor = persistStore(store);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage  from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import { cricketerSlice } from './cricketer-slice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducer = combineReducers({
+  cricketers: cricketerSlice.reducer 
+})
+
+const persistedReducer  = persistReducer(persistConfig,reducer)
+
 export const store = configureStore({
-  reducer: { cricketers: cricketerSlice.reducer  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
